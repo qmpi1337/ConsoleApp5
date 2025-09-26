2c552a0a54c57c5a13c5a87dbc19a3df713966fe
315 <315@315-44>
2025-09-26 10:30:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Employee
+{
+    public int ID { get; set; } // ID 
+    public string FIO { get; set; } // ФИО 
+    public string Position { get; set; } // Должность
+    public int Salary { get; set; } // ЗП
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        List<Employee> Company = new List<Employee>();
+        
+
+    }
+}
